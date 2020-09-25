01818cdd1b72a1c48e953185ced6a8e4941d7838
Chris Dosé <chris.dose@gmail.com>
2020-09-24 21:09:49 -0700

    Initial commit

---
+++ Ship.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+
+var screen_size  # Size of the game window.
+
+var velocity = Vector2()
+
+const ROTATION_SPEED = PI / 64
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		rotation += ROTATION_SPEED
+	if Input.is_action_pressed("ui_left"):
+		rotation -= ROTATION_SPEED
+
+	if Input.is_action_pressed("ui_up"):
+		var new_velocity = Vector2(5, 0).rotated(rotation)
+		velocity += new_velocity
+
+	position += velocity * delta
+
+	position.x = wrapf(position.x, 0, screen_size.x)
+	position.y = wrapf(position.y, 0, screen_size.y)
