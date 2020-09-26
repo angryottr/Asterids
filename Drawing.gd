abb1af51010b2ce0914de57cf82152bb710ba6c0
Chris Dosé <chris.dose@gmail.com>
2020-09-26 11:21:58 -0700

    Configurable ship drawing size

--- Drawing.gd
+++ Drawing.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 const color = Color(1.0, 1.0, 1.0)
+export var size = 16
 
 func _draw():
-	draw_line(Vector2(0, -32), Vector2(-32, 32), color)
-	draw_line(Vector2(0, -32), Vector2(32, 32), color)
-	draw_line(Vector2(-32, 32), Vector2(0, 16), color)
-	draw_line(Vector2(32, 32), Vector2(0, 16), color)
+	draw_line(Vector2(0, -size), Vector2(-size, size), color)
+	draw_line(Vector2(0, -size), Vector2(size, size), color)
+	draw_line(Vector2(-size, size), Vector2(0, size / 2), color)
+	draw_line(Vector2(size, size), Vector2(0, size / 2), color)
 
 
 func _process(delta):
